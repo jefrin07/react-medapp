7864074b2eea67aa3effec7bf85e4235fd1a1572
Jefrin <115068845+jefrin07@users.noreply.github.com>
2024-04-08 18:58:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import checkAuth from './components/auth/checkAuth';
+function App() {
+  return(
+  <div>
+    <Navbar></Navbar>
+    <div className="row d-flex justify-content-center mt-3">
+      <h2>WELCOME!!</h2>
+    </div>
+  </div>
+);
+}
+
+export default checkAuth(App);
